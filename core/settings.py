ea73fc617b3ae0f30f821628f8376671d87e1c2a
luzza07 <luzzadangol456@gmail.com>
2025-03-17 17:35:52 +0545

    feat:auth using pyjwt

--- core/settings.py
+++ core/settings.py
@@ -9,18 +9,22 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY=os.getenv('SECRET_KEY')
+JWT_SECRET_KEY=os.getenv('JWT_SECRET_KEY')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+n&7+4wyg+$f#4clk0j2l*zxfz$n0*4m$=vk2=@@=7rm&#+uc='
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -72,6 +76,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+load_dotenv()
 
 DATABASES = {
 
